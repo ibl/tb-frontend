0bed3e7b1c542ef7884869a3591a69985c1d4fad
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2013-09-23 13:02:31 -0500

    Kickstart repo.

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        connect: {
+            server: {
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            files: ["index.html", "scripts/**/*"]
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-connect");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.registerTask("default", ["connect", "watch"]);
+
+};
