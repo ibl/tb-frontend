21ccd6437c7a60ef7c7d314f80f9dd525d1fb9af
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2013-09-25 17:32:08 -0500

    Add patient list.

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,39 @@
 (function () {
     "use strict";
 
-    var app;
+    var backend, app;
 
-    app = angular.module("app", []);
+    backend = "http://hydrogen.path.uab.edu/tb/api/v1";
+
+    app = angular.module("app", ["ngResource"]);
 
     app.config(function ($routeProvider) {
         $routeProvider.when("/", {
             templateUrl: "templates/index.html",
+        }).when("/patients", {
+            templateUrl: "templates/patients/list.html",
+            controller: "PatientListController",
+            resolve: {
+                patients: function ($q, Patient) {
+                    var deferred;
+                    deferred = $q.defer();
+                    Patient.query(function (patients) {
+                        deferred.resolve(patients);
+                    });
+                    return deferred.promise;
+                }
+            }
         });
+    });
+
+    app.factory("Patient", function ($resource) {
+        return $resource(backend + "/patients/:id", {
+            id: "@_id"
+        });
+    });
+
+    app.controller("PatientListController", function ($scope, patients) {
+        $scope.patients = patients;
     });
 
 }());
