5b29b79dc3693da251ca6c585821d55767db58b5
David Robbins <robbins.david@gmail.com>
2013-12-17 11:36:03 -0600

    Initial search functionality

--- scripts/app.js
+++ scripts/app.js
@@ -257,7 +257,7 @@
         };
     });
 
-    app.controller("ListPatientsController", function ($scope, patients, $stateParams) {
+    app.controller("ListPatientsController", function ($scope, patients, $stateParams, Patient) {
         $scope.patients = patients;
         $scope.limit = parseInt($stateParams.limit, 10) || defaultLimit;
         $scope.skip = parseInt($stateParams.skip, 10) || 0;
@@ -269,6 +269,27 @@
             $scope.pages.push({
                 page: (offset)/$scope.limit + 1,
                 skip: offset
+            });
+        }
+
+        $scope.searchPatients = function (term) {
+            $scope.patients = Patient.query({
+                conditions: JSON.stringify({
+                    "$or": [
+                        {
+                            "name": {
+                                "$regex": term,
+                                "$options": "i"
+                            }
+                        },
+                        {
+                            "mrn": {
+                                "$regex": term,
+                                "$options": "i"
+                            }
+                        }
+                    ]
+                })
             });
         }
     });
